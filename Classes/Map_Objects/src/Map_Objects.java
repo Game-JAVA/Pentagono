f094882bc89451427ff751064edc31f669f407dd
GabrielEliezer <g.eliezer.rodrigues@gmail.com>
2024-06-12 17:44:20 -0300

    Docs: sendings codes

---
+++ Classes/Map_Objects/src/Map_Objects.java
@@ -0,0 +1,21 @@
+public abstract class Map_Objects {
+
+
+    //ATRIBUTOS
+private String object_name;
+private String object_type;
+
+private int pos_x;
+private int pos_y;
+
+    //METODOS
+
+
+
+    //GETTERS AND SETTERS
+
+
+
+
+
+}
