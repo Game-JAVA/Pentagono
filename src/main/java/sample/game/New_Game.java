0bafabcd603415276317188a7aafa2ce103108d3
Henrique Fernandes <henriquealcalde@hotmail.com>
2024-06-28 14:33:43 -0300

    feat(newgame):corrify colision
    verify colision between character and board's limit

--- src/main/java/sample/game/New_Game.java
+++ src/main/java/sample/game/New_Game.java
@@ -120,7 +120,6 @@ public class New_Game extends Application {
                 "-fx-text-fill: white;"+
                         "-fx-font-size: 26px;"+
                         "-fx-padding: 10 0 0 60"
-
         );
 
         Label label3 = new Label("SCORE:");
@@ -203,7 +202,6 @@ public class New_Game extends Application {
 
 
         //Implemented Shield in Window
-
         shield.setPos_y(MO.aleatoryPositionY() * squareSize + startY);
         shield.setPos_x(MO.aleatoryPositionX() * squareSize + startX);
 
@@ -212,7 +210,6 @@ public class New_Game extends Application {
 
 
         //Implemented Boots in window.
-
         boots.setPos_y(MO.aleatoryPositionY() * squareSize + startY);
         boots.setPos_x(MO.aleatoryPositionX() * squareSize + startX);
 
@@ -221,14 +218,11 @@ public class New_Game extends Application {
 
 
         //BOMB IMPLEMENTATION
-
         bomb.setPos_y(MO.aleatoryPositionY() * squareSize + startY);
         bomb.setPos_x(MO.aleatoryPositionX() * squareSize + startX);
 
         bombRectangle.setX(bomb.getPos_x());
         bombRectangle.setY(bomb.getPos_y());
-
-
 
         // Log positions
         System.out.println("Character initial position: x = " + character.getPos_x() + ", y = " + character.getPos_y());
@@ -239,8 +233,6 @@ public class New_Game extends Application {
 
         // Rectangle's Movement
         s2.setOnKeyPressed(event -> {
-            int characterPosX = character.getPos_x();
-            int characterPosY = character.getPos_y();
             int applePosX = a1.getPos_x();
             int applePosY = a1.getPos_y();
 
@@ -270,7 +262,7 @@ public class New_Game extends Application {
             }
 
             // Check if character and apple occupy the same square
-            if (characterPosX == applePosX && characterPosY == applePosY) {
+            if (character.getPos_x() == applePosX && character.getPos_y() == applePosY) {
                 // Reset apple position
                 character.setScore(a1.getScore());
                 label4.setText(String.valueOf(character.getScore()));
@@ -285,11 +277,11 @@ public class New_Game extends Application {
                 delayTimeline.play();
             }
 
-            if(characterPosX == bootsRectangle.getX() && characterPosY == bootsRectangle.getY()){
-                bootsRectangle.setX(MO.aleatoryPositionX()*squareSize + startY);
-                bootsRectangle.setY(MO.aleatoryPositionY()*squareSize + startX);
+            if(character.getPos_x() == bootsRectangle.getX() && character.getPos_y() == bootsRectangle.getY()){
                 bootsRectangle.setVisible(false);
                 character.setSpeedMore();
+                bootsRectangle.setX(MO.aleatoryPositionX()*squareSize + startY);
+                bootsRectangle.setY(MO.aleatoryPositionY()*squareSize + startX);
                 Timeline delayTimeline = new Timeline(
                         new KeyFrame(Duration.seconds(3), e -> {
                             boots.setPos_x(MO.aleatoryPositionX() * squareSize + startX);
@@ -301,7 +293,7 @@ public class New_Game extends Application {
                 delayTimeline.play();
             }
 
-            if(characterPosX == shieldRectangle.getX() && characterPosY == shieldRectangle.getY()){
+            if(character.getPos_x() == shieldRectangle.getX() && character.getPos_y() == shieldRectangle.getY()){
                 shieldRectangle.setX(MO.aleatoryPositionX()*squareSize + startY);
                 shieldRectangle.setY(MO.aleatoryPositionY()*squareSize + startX);
                 shieldRectangle.setVisible(false);
@@ -319,15 +311,6 @@ public class New_Game extends Application {
             System.out.println("Character moved to: x = " + character.getPos_x() + ", y = " + character.getPos_y() + ", Score = " + character.getScore());
             System.out.println("Apple position is : x = " + a1.getPos_x() + ", y = " + a1.getPos_y());
         });
-
-        try {
-            String css = getClass().getResource("style.css").toExternalForm();
-            s1.getStylesheets().add(css);
-            s2.getStylesheets().add(css);
-        } catch (NullPointerException e) {
-            System.err.println("Arquivo CSS não encontrado. Verifique o caminho: " + e.getMessage());
-            e.printStackTrace();
-        }
 
         // Build the Window
         primaryStage.setScene(s1);
