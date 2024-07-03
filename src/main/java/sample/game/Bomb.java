c2bf4d27dec0085cb9d2368943449c977917eadc
GabrielEliezer <g.eliezer.rodrigues@gmail.com>
2024-07-03 15:30:17 -0300

    feat(bomb): change signature of constructor
    The constructor has been changed to generate random positions on the map for the bomb

--- src/main/java/sample/game/Bomb.java
+++ src/main/java/sample/game/Bomb.java
@@ -1,25 +1,28 @@
 package sample.game;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import java.util.Random;
 
-public class Bomb extends Map_Objects{
+public class Bomb extends ImageView {
 
     private int explosion_range;
     private int explosion_diameter;
     private int explosion_damage;
-    private int pos_x;
-    private int pos_y;
-    private Color color;
+    private int posx;
+    private int posy;
+    private int char_posx;
+    private int char_posy;
+    private String imagePath;
     Random r = new Random();
 
     // constructor
-    public Bomb(int width, int height, int pos_x, int pos_y, Color color) {
-        this.explosion_range = explosion_range;
-        this.explosion_diameter = 40;
-        this.explosion_damage = explosion_damage;
-        this.pos_x = r.nextInt(width);
-        this.pos_y = r.nextInt(height);
-        this.color=color;
+    public Bomb(int width, int height, int posx,int posy, String imagePath) {
+        this.setFitHeight(height);
+        this.setFitWidth(width);
+        this.setImage(new Image(imagePath));
+        this.setX(posx*40);
+        this.setY(posy*40);
     }
 
     // getters and setters
@@ -36,28 +39,32 @@ public class Bomb extends Map_Objects{
     }
 
     public int getPos_x() {
-        return pos_x;
+        return posx;
     }
     public int getPos_y() {
-        return pos_y;
+        return posy;
     }
 
-    public Color getColor() {
-        return color;
-    }
 
     public void setPos_x(int pos_x) {
-        this.pos_x = pos_x;
+        this.posx = pos_x;
     }
 
     public void setPos_y(int pos_y) {
-        this.pos_y = pos_y;
+        this.posy = pos_y;
     }
 
     public void setR(Random r) {
         this.r = r;
     }
     // methods
+
+    public void explosion(){
+            System.out.println("você morreu");
+    }
+
+
+
 
     // toString
 
@@ -68,8 +75,8 @@ public class Bomb extends Map_Objects{
                 "explosion_range=" + explosion_range +
                 ", explosion_diameter=" + explosion_diameter +
                 ", explosion_damage=" + explosion_damage +
-                ", pos_x=" + pos_x +
-                ", pos_y=" + pos_y +
+                ", posx=" + posx +
+                ", posy=" + posy +
                 '}';
     }
 }
